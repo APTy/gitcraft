f7bbea10db97e3bd47ced1ee8747708ea56d2454
R. Tyler Julian <trjulian@gmail.com>
2015-04-17 16:03:03 -0700

    Initial commit

---
+++ client/sc.js
@@ -0,0 +1,27 @@
+// sc.js
+
+var socket = io('http://fu.ngrok.com');
+socket.on('pull', function (data) {
+  playRandom();
+});
+
+var soundFile = document.createElement("audio");
+soundFile.src = 'audio/sc-marine.mp3';
+document.body.appendChild(soundFile);
+
+// var times = [.01, 4.5, 6, 9, 12, 14, 16, 19, 21, 24, 27, 31, 34, 37, 42, 46, 50, 53, 58];
+// playSection(times[times.length - 2], times[times.length - 1]);
+var times = [.01, 4.5, 6, 9, 12, 14, 16, 19, 21, 24, 27, 31, 34, 37];
+
+function playSection(start, end) {
+  soundFile.currentTime = start;
+  soundFile.play();
+  setTimeout(function() {
+    soundFile.pause();
+  }, (end-start)*1000);
+}
+
+function playRandom() {
+  var rnd = Math.floor(Math.random() * (times.length - 1));
+  playSection(times[rnd], times[rnd+1]);
+}
---
+++ server/server.js
@@ -0,0 +1,30 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+var createHandler = require('github-webhook-handler');
+var handler = createHandler({ path: '/webhook', secret: 'oath' });
+
+app.listen(3420);
+
+function handler (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404;
+    res.end('no such location');
+  });
+}
+
+io.on('connection', function (socket) {
+  console.log('connected');
+});
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+});
+
+handler.on('pull_request', function (event) {
+  console.log('pull_request');
+  // if (event.payload.action === 'opened') {
+    console.log('emitting pull request to sockets');
+    io.emit('pull');
+  // }
+});
